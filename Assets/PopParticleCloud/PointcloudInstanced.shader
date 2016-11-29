03664b66ecc8122261b19da03011aaf4025e90a7
Graham Reeves <graham@grahamreeves.com>
2016-11-29 19:46:42 +0000

    Added instanced renderer for dumb mesh N times.  Very fast. 5.5 required.

---
+++ Assets/PopParticleCloud/PointcloudInstanced.shader
@@ -0,0 +1,135 @@
+﻿// Upgrade NOTE: replaced 'UNITY_INSTANCE_ID' with 'UNITY_VERTEX_INPUT_INSTANCE_ID'
+
+Shader "Unlit/PointcloudInstanced"
+{
+	Properties
+	{
+		ParticleSize("ParticleSize",Range(0.001,1) ) = 0.1
+		Radius("Radius", Range(0,1) ) = 0.5
+		UseVertexColour("UseVertexColour", Range(0,1) ) = 0.0
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+		Cull off
+		Blend Off
+
+		Pass
+		{
+		CGPROGRAM
+
+			//	geometry shader needs GL ES 3+
+			//	https://docs.unity3d.com/Manual/SL-ShaderCompileTargets.html
+			#pragma target 3.5
+
+			#pragma vertex vert
+			#pragma fragment frag
+			#pragma multi_compile_instancing
+
+			#include "UnityCG.cginc"
+
+			#define lengthsq(x)	dot( (x), (x) )
+			#define squared(x)	( (x)*(x) )
+
+
+			struct app2vert
+			{
+				float4 LocalPos : POSITION;
+				fixed4 Rgba : COLOR;
+				fixed3 Normal : NORMAL;
+				UNITY_VERTEX_INPUT_INSTANCE_ID
+			};
+
+			struct FragData
+			{
+				float4 ScreenPos : SV_POSITION;
+				fixed3 LocalOffset : TEXCOORD0;
+				fixed3 Colour : TEXCOORD1;
+				UNITY_VERTEX_INPUT_INSTANCE_ID
+			};
+
+			UNITY_INSTANCING_CBUFFER_START (MyProperties)
+			//	UNITY_DEFINE_INSTANCED_PROP (float4, _Color)
+			UNITY_INSTANCING_CBUFFER_END
+
+			float ParticleSize;
+			float Radius;
+			float UseVertexColour;
+
+			fixed3 GetParticleSize3()
+			{
+				return float3(ParticleSize,ParticleSize,ParticleSize);
+				//	gr: thought it length of each row was scale but... doesn't seem to be. row vs col major issue?
+				//float WorldScale = length(unity_ObjectToWorld[0]) + length(unity_ObjectToWorld[1]) + length(unity_ObjectToWorld[2]);
+				//WorldScale /= 3.0;
+
+				fixed3 OneTrans = mul( unity_ObjectToWorld, float4(1,0,0,0 ) );
+				fixed3 ZeroTrans = mul( unity_ObjectToWorld, float4(0,0,0,0 ) );
+				float WorldScale = length(OneTrans - ZeroTrans);
+				fixed3 ParticleSize3 = float3( ParticleSize * WorldScale, ParticleSize * WorldScale, ParticleSize * WorldScale );
+				return ParticleSize3;
+			}
+
+
+			FragData MakeFragData(fixed3 TriangleIndexer,float3 input_WorldPos,fixed3 input_Rgba,fixed3 ParticleSize3)
+			{
+				fixed isa = TriangleIndexer.x ;
+				fixed isb = TriangleIndexer.y ;
+				fixed isc = TriangleIndexer.z ;
+
+				FragData x = (FragData)0;
+
+				//	gr: use maths to work out biggest circle 
+				fixed Top = -0.6;
+				fixed Bottom = 0.3;
+				fixed Width = 0.5;
+				x.LocalOffset = isa * fixed3( 0,Top,0 );
+				x.LocalOffset += isb * fixed3( -Width,Bottom,0 );
+				x.LocalOffset += isc * fixed3( Width,Bottom,0 );
+
+				float3 x_WorldPos = mul( UNITY_MATRIX_V, float4(input_WorldPos,1) ) + (x.LocalOffset * ParticleSize3);
+				//float3 x_WorldPos = mul( UNITY_MATRIX_V, float3(0,0,0) ) + (x.LocalOffset * ParticleSize3);
+				x.ScreenPos = mul( UNITY_MATRIX_P, float4(x_WorldPos,1) );
+				x.Colour = TriangleIndexer;
+				x.Colour = lerp( x.Colour, input_Rgba, UseVertexColour );
+				return x;
+			}
+
+			FragData vert(app2vert v)
+			{
+				FragData o;
+				UNITY_SETUP_INSTANCE_ID (v);
+				UNITY_TRANSFER_INSTANCE_ID (v, o);
+
+				float4 LocalPos = v.LocalPos;
+				
+				float3 WorldPos = mul( unity_ObjectToWorld, LocalPos );
+
+				fixed3 ParticleSize3 = GetParticleSize3();
+
+				o = MakeFragData( v.Normal, WorldPos, v.Rgba.xyz, ParticleSize3 );
+
+				//o.ScreenPos = UnityObjectToClipPos( LocalPos );
+
+
+				return o;
+			}
+
+
+			fixed4 frag(FragData i) : SV_Target
+			{
+				UNITY_SETUP_INSTANCE_ID (i); 
+				//return UNITY_ACCESS_INSTANCED_PROP (_Color);
+
+				float DistanceFromCenterSq = lengthsq(i.LocalOffset);
+
+				if ( DistanceFromCenterSq > squared(Radius) )
+					discard;
+
+				return fixed4( i.Colour, 1 );
+			}
+				ENDCG
+		}
+	}
+}
---
+++ Assets/PopParticleCloud/PopCloudInstanceRenderer.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class PopCloudInstanceRenderer : MonoBehaviour {
+
+	[InspectorButton("Generate")]
+	public bool	Dirty = true;
+
+	[InspectorButton("GenerateChildren")]
+	public bool	DirtyChildren = true;
+	public bool	AlwaysDirtyChildren = true;
+
+	[InspectorButton("EnableChildren")]
+	public bool	EnableChildrenx = true;
+
+
+	public List<Matrix4x4>	InstanceMatrixes;
+
+	void Update () {
+
+		if (DirtyChildren)
+		{
+			GenerateChildren();
+
+			if ( !AlwaysDirtyChildren )
+				DirtyChildren = false;
+		}
+
+
+		if (Dirty)
+		{
+			Generate();
+			Dirty = false;
+		}
+
+		RenderInstances();
+	}
+
+	public void RenderInstances()
+	{
+		var mf = GetComponent<MeshFilter>();
+		var mr = GetComponent<MeshRenderer>();
+
+		var CloudMaterial = GetComponent<MeshRenderer>();
+
+		//Debug.Log("Rendering " + InstanceMatrixes.Count + " isntances");
+
+        Graphics.DrawMeshInstanced( mf.sharedMesh, 0, mr.sharedMaterial, InstanceMatrixes );
+
+		//	now we're drawing the isntances, don't draw self
+		mr.enabled = false;
+    }
+
+	public void Generate()
+	{
+		var PointMesh = new Mesh();
+
+		PointMesh.name = this.name;
+
+		var Positions = new List<Vector3> ();
+		var Normals = new List<Vector3> ();
+		var Colours = new List<Color> ();
+		bool BoundsInitialised = false;
+		Bounds MinMax = new Bounds ();
+
+		var MeshWidth = 147;
+		var MeshHeight = 147;
+
+		//	decode points
+		{
+			var Pos3 = new Vector3 ();
+			var Colour3 = new Color();
+			var Space = new char[]{ ' ' };
+			/*
+			Vector3 Offa = new Vector3(0.01f,0,0);
+			Vector3 Offb = new Vector3(0,0.01f,0);
+			Vector3 Offc = new Vector3(0,0,0.01f);
+			*/
+			Vector3 Offa = Vector3.zero;
+			Vector3 Offb = Vector3.zero;
+			Vector3 Offc = Vector3.zero;
+
+			for ( var x=0;	x<MeshWidth;	x++ )
+			{
+				for ( var y=0;	y<MeshHeight;	y++ )
+				{
+					Pos3.x = x / (float)MeshWidth;
+					Pos3.z = y / (float)MeshHeight;
+					Pos3.y = Mathf.PerlinNoise( Pos3.x, Pos3.z );
+
+					Colour3.r = Pos3.x;
+					Colour3.g = Pos3.z;
+					Colour3.b = 0;
+
+					if ( !BoundsInitialised )
+					{
+						MinMax = new Bounds (Pos3, Vector3.zero );
+						BoundsInitialised = true;
+					}
+					MinMax.Encapsulate( Pos3 );
+
+
+					Positions.Add( Pos3+Offa );
+					Colours.Add( Colour3 );
+					Normals.Add( new Vector3(1,0,0) );
+
+					Positions.Add( Pos3+Offb );
+					Colours.Add( Colour3 );
+					Normals.Add( new Vector3(0,1,0) );
+
+					Positions.Add( Pos3+Offc );
+					Colours.Add( Colour3 );
+					Normals.Add( new Vector3(0,0,1) );
+				} 
+			}
+					
+		}
+
+		//  generate indicies 
+		var Indexes = new int[Positions.Count]; 
+		for (int i = 0;  i < Indexes.Length;  i++) 
+			Indexes [i] = i; 
+
+		PointMesh.SetVertices( Positions );
+		PointMesh.SetNormals (Normals);
+		PointMesh.SetColors (Colours);
+		PointMesh.SetIndices (Indexes, MeshTopology.Triangles, 0);
+		PointMesh.bounds = MinMax;
+
+		var mf = GetComponent<MeshFilter>();
+		mf.sharedMesh = PointMesh;
+	}
+
+	public void GenerateChildren()
+	{
+		InstanceMatrixes = new List<Matrix4x4>();
+		var Children = GetComponentsInChildren<MeshRenderer>(true);
+		foreach ( var Child in Children )
+		{
+			if ( Child.transform == this.transform )
+				continue;
+
+			InstanceMatrixes.Add( Child.transform.localToWorldMatrix );
+
+			Child.gameObject.SetActive(false);
+		}
+	}
+
+	public void EnableChildren()
+	{
+		var Children = GetComponentsInChildren<MeshRenderer>(true);
+		foreach ( var Child in Children )
+		{
+			if ( Child.transform == this.transform )
+				continue;
+
+			Child.gameObject.SetActive(true);
+		}
+	}
+	
+
+}
